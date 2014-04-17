1edc9df2782d4e675183e97a3d7c36f47aa73871
Chris Lansing <cslansing@localhost.localdomain>
2014-04-16 17:24:35 -0700

    moved py venv settings from bash_profile to bashrc

--- bashrc
+++ bashrc
@@ -8,3 +8,8 @@ fi
 # User specific aliases and functions
 alias ls="ls -h --color=auto"
 export PS1='\[\033[01;30m\]\u@\h\n\[\033[00;31m\]\w \$ \[\033[00m\]'
+
+# location of python virtual envs
+export WORKON_HOME=$HOME/.virtualenvs
+# virtualenvwrapper install location
+source /usr/bin/virtualenvwrapper.sh
--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile vimrc vim Xdefaults i3 i3status.conf"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim Xdefaults i3 i3status.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
