5cfc692e7d7e70b5ce5793d3789bc957579c33db
Chris Lansing <cslansin@gmail.com>
2015-10-02 10:40:06 -0400

    changing color scheme to tomorrow-night-80s

--- gtree.sh
+++ gtree.sh
@@ -1,1 +1,1 @@
-git log --graph --full-history --max-count=50 --all --color --abbrev-commit --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"
+git log --graph --full-history --max-count=100 --all --color --abbrev-commit --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"
